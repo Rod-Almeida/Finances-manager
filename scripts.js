f52c01c730a737aee425353034911cd1b624be20
Rodrigo Almeida <rod.almeida01@gmail.com>
2021-02-11 13:32:27 -0300

    Bug fix (Math.round)

--- scripts.js
+++ scripts.js
@@ -116,9 +116,9 @@ const DOM = {
 const Utils = {
 
     formatAmount(value){
-        value = Number(value) * 100
+        value = value * 100
         
-        return value
+        return Math.round(value)
     },
 
     formatDate(date){
